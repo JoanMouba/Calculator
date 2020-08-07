68863aa099870c510e138fd93ef43e4279462d57
JoanMouba <joan.mouba@gmail.com>
2020-08-07 22:43:22 +0200

    JM: add gui.py and tests

--- calculator.py
+++ calculator.py
@@ -1,2 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+def addition(a, b):
+    """ Addition of 2 numbers """
+    return a + b
+
 if __name__ == '__main__':
+    a, b= (10, 20)
+    print("{}+{}={}".format(a, b, addition(a, b)))
     print("Ready for your computations")
--- tests.py
+++ tests.py
@@ -1,10 +1,18 @@
 import unittest
-
+import calculator
 
 class CalculatorTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def setUp(self):
+        print("SETUP CALLED...")
+        self.a, self.b = (10, 99)
 
+    def tearDown(self):
+        print("TEAR DOWN CALLED...")
+        a=b=0
+
+    def test_addition(self):
+        print("TEST ADDITION")
+        self.assertEqual(calculator.addition(self.a, self.b), 109)
 
 if __name__ == '__main__':
     unittest.main()
